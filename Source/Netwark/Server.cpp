c98014e62a091d6a8e8455112176b3ccb07e5387
ROKOina <suzumiyahuusuke@gmail.com>
2024-08-02 19:49:47 +0900

    ネットテスト１５

--- Source/Netwark/Server.cpp
+++ Source/Netwark/Server.cpp
@@ -303,7 +303,6 @@ void NetServer::Send()
         inputUp = 0;
 
         client.nowFrame = nowFrame;
-        client.playDelay = playFrame;
         client.damageData = player->GetComponent<CharacterCom>()->GetGiveDamage();
 
         break;
