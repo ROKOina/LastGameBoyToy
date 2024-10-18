1832b8129725d75c530b854e85efd81ade550104
MSI\2210454 <yito2881@gmail.com>
2024-10-18 10:11:16 +0900

    マージ打消し

---
+++ Source/Components/RigidBodyCom.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Phsix\Physxlib.h"
+#include "System\GameObject.h"
+#include "System\Component.h"
+#include "Components\NodeCollsionCom.h"
+
+class RigidBodyCom : public Component
+{
+public:
+    RigidBodyCom(bool isStatic, NodeCollsionCom::CollsionType type);
+    ~RigidBodyCom() {}
+
+    // 名前取得
+    const char* GetName() const override { return "RigidBody"; }
+
+    // 開始処理
+    void Start() override;
+
+    // 更新処理
+    void Update(float elapsedTime) override;
+
+    // GUI描画
+    void OnGUI() override;
+
+private:
+    bool isStatic = false;
+    NodeCollsionCom::CollsionType type = NodeCollsionCom::CollsionType::MAX;
+
+    physx::PxRigidActor* rigidActor = nullptr;
+
+    physx::PxTransform rigidTransform = {};
+};
+
