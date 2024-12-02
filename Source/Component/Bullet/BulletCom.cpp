26d169eedbb5f9fff440045601a4a074a606d06b
yusaku12 <2210359@ecc.ac.jp>
2024-12-02 23:21:07 +0900

    ふざけるな

--- Source/Component/Bullet/BulletCom.cpp
+++ Source/Component/Bullet/BulletCom.cpp
@@ -46,6 +46,15 @@ void BulletCom::Update(float elapsedTime)
         std::shared_ptr<GameObject> nonCharaObj;
         if (GetGameObject()->GetComponent<HitProcessCom>()->IsHit())
         {
+            //ヒットエフェクト生成
+            std::shared_ptr<GameObject> hiteffectobject = GameObjectManager::Instance().Create();
+            hiteffectobject->transform_->SetWorldPosition(GetGameObject()->transform_->GetWorldPosition());
+            hiteffectobject->SetName("HitEffect");
+            std::shared_ptr<GPUParticle>Chiteffct = hiteffectobject->AddComponent<GPUParticle>("Data/SerializeData/GPUEffect/hanabi.gpuparticle", 1000);
+            Chiteffct->Play();
+            std::shared_ptr<CPUParticle>Ghiteffct = hiteffectobject->AddComponent<CPUParticle>("Data/SerializeData/CPUEffect/hitsmokeeffect.cpuparticle", 100);
+            Ghiteffct->SetActive(true);
+
             GameObjectManager::Instance().Remove(this->GetGameObject());
             GameObjectManager::Instance().Remove(viewBullet.lock());
         }
