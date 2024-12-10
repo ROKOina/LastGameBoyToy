296d5eacae4dfe25880428d7dd7a639696add2fa
ライック <88312556+raikku@users.noreply.github.com>
2024-12-10 10:23:49 +0900

    一旦コミット

---
+++ Source/Audio/3DAudio/Audio3d.h
@@ -0,0 +1,17 @@
+#pragma once
+#pragma comment(lib, "xaudio2.lib")
+
+#include <xaudio2.h>
+#include <x3daudio.h>
+#include <memory>
+
+// ‘O•ûéŒ¾
+class AudioSource;
+
+class Audio3d
+{
+public:
+    Audio3d() = default;
+    ~Audio3d() = default;
+};
+
