d41a34369d055ecf068aad0fa763de1abf06d0b5
ライック <88312556+raikku@users.noreply.github.com>
2024-12-20 16:43:02 +0900

    クリックするとスキル表示、キャラ名、選択キャラ、アイコンが表示

--- Source/PvPUi/CharaPicks.cpp
+++ Source/PvPUi/CharaPicks.cpp
@@ -35,8 +35,6 @@ void CharaPicks::CreateCharaPicksUiObject()
                 skill->AddComponent<Sprite>("Data/SerializeData/UIData/CharaPick/charaSkill0.ui", Sprite::SpriteShader::DEFALT, false);
                 skill->SetEnabled(false);
             }
-
-
         }
         // FARAH
         {
@@ -49,7 +47,7 @@ void CharaPicks::CreateCharaPicksUiObject()
                 auto& name = chara->AddChildObject();
                 name->SetName("name");
                 name->AddComponent<Sprite>("Data/SerializeData/UIData/CharaPick/charaName1.ui", Sprite::SpriteShader::DEFALT, false);
-                name->GetComponent<Sprite>()->SetEnabled(false);
+                name->SetEnabled(false);
             }
 
 
@@ -58,7 +56,7 @@ void CharaPicks::CreateCharaPicksUiObject()
                 auto& skill = chara->AddChildObject();
                 skill->SetName("skill");
                 skill->AddComponent<Sprite>("Data/SerializeData/UIData/CharaPick/charaSkill1.ui", Sprite::SpriteShader::DEFALT, false);
-                skill->GetComponent<Sprite>()->SetEnabled(false);
+                skill->SetEnabled(false);
             }
 
         }
@@ -73,7 +71,7 @@ void CharaPicks::CreateCharaPicksUiObject()
                 auto& name = chara->AddChildObject();
                 name->SetName("name");
                 name->AddComponent<Sprite>("Data/SerializeData/UIData/CharaPick/charaName2.ui", Sprite::SpriteShader::DEFALT, false);
-                name->GetComponent<Sprite>()->SetEnabled(false);
+                name->SetEnabled(false);
             }
 
             // スキル表記
@@ -81,7 +79,7 @@ void CharaPicks::CreateCharaPicksUiObject()
                 auto& skill = chara->AddChildObject();
                 skill->SetName("skill");
                 skill->AddComponent<Sprite>("Data/SerializeData/UIData/CharaPick/charaSkill2.ui", Sprite::SpriteShader::DEFALT, false);
-                skill->GetComponent<Sprite>()->SetEnabled(false);
+                skill->SetEnabled(false);
             }
 
         }
@@ -96,7 +94,7 @@ void CharaPicks::CreateCharaPicksUiObject()
                 auto& name = chara->AddChildObject();
                 name->SetName("name");
                 name->AddComponent<Sprite>("Data/SerializeData/UIData/CharaPick/charaName3.ui", Sprite::SpriteShader::DEFALT, false);
-                name->GetComponent<Sprite>()->SetEnabled(false);
+                name->SetEnabled(false);
             }
 
             // スキル表記
@@ -104,7 +102,7 @@ void CharaPicks::CreateCharaPicksUiObject()
                 auto& skill = chara->AddChildObject();
                 skill->SetName("skill");
                 skill->AddComponent<Sprite>("Data/SerializeData/UIData/CharaPick/charaSkill3.ui", Sprite::SpriteShader::DEFALT, false);
-                skill->GetComponent<Sprite>()->SetEnabled(false);
+                skill->SetEnabled(false);
             }
 
         }
@@ -128,7 +126,7 @@ void CharaPicks::CharaPicksUpdate(float elapsedTime)
 {
     // 各キャラ詳細
     CharaDetails();
-    
+
     // 時間制限システム
     TimeLimitSystem(elapsedTime);
 }
@@ -140,34 +138,45 @@ void CharaPicks::CharaDetails()
     GamePad& gamePad = Input::Instance().GetGamePad();
 
     // アイコンを選択したら表示
-    {
-        auto& canvas = GameObjectManager::Instance().Find("CharaPicksCanvas");
+    auto& canvas = GameObjectManager::Instance().Find("CharaPicksCanvas");
 
-        {
-            auto& chara = canvas->GetChildFind("INAZAWA");
-            auto& sprite = chara->GetComponent<Sprite>();
+    auto getCharacterInfo = [&](const std::string& charaName, int id) -> CharacterInfo {
+        auto& chara = canvas->GetChildFind(charaName.c_str());
+        return {
+            chara,
+            chara->GetComponent<Sprite>(),
+            chara->GetChildFind("name"),
+            chara->GetChildFind("skill"),
+            id
+        };
+        };
 
-            auto& name = chara->GetChildFind("name");
-            auto& skill = chara->GetChildFind("skill");
+    std::vector<CharacterInfo> characters = {
+        getCharacterInfo("INAZAWA", 0),
+        getCharacterInfo("FARAH", 1),
+        getCharacterInfo("JANKRAT", 2),
+        getCharacterInfo("chara4", 3)
+    };
 
-            // スプライト判定
-            if (sprite->GetHitSprite())
-            {
-                name->SetEnabled(true);
-                skill->SetEnabled(true);
-            }
-            else
-            {
-                name->SetEnabled(false);
-                skill->SetEnabled(false);
+    auto handleCharacterSelection = [&](CharacterInfo& selected, std::vector<CharacterInfo>& others) {
+        if (GamePad::BTN_RIGHT_TRIGGER & gamePad.GetButtonDown() && selected.sprite->GetHitSprite()) {
+            selected.name->SetEnabled(true);
+            //selected.skill->SetEnabled(true);
+            selectedCharacterId = selected.id;
+
+            for (auto& other : others) {
+                if (&other != &selected) {
+                    other.name->SetEnabled(false);
+                    other.skill->SetEnabled(false);
+                }
             }
         }
-    }
+        };
 
-
-    if (GamePad::BTN_RIGHT_TRIGGER & gamePad.GetButtonDown())
-    {
-
+    for (size_t i = 0; i < characters.size(); ++i) {
+        std::vector<CharacterInfo> others = characters;
+        others.erase(others.begin() + i);
+        handleCharacterSelection(characters[i], others);
     }
 }
 
--- Source/PvPUi/CharaPicks.h
+++ Source/PvPUi/CharaPicks.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <memory>
+
+// 前方宣言
+class GameObject;
+class Sprite;
 
 class CharaPicks
 {
@@ -14,6 +19,8 @@ public:
     // キャラピック表示設定
     void SetViewCharaPicks(bool flg);
 
+    // 選択されたキャラのIDを取得
+    int GetSelectedCharacterId() const { return selectedCharacterId; }
 private:
     // キャラ詳細（スキル表示、キャラ名、選択キャラ表示）
     void CharaDetails();
@@ -24,5 +31,17 @@ private:
     // 時間制限
     void TimeLimitSystem(float elapsedTime);
 
+private:
+    struct CharacterInfo {
+        std::shared_ptr<GameObject> chara;
+        std::shared_ptr<Sprite> sprite;
+        std::shared_ptr<GameObject> name;
+        std::shared_ptr<GameObject> skill;
+        int id;
+    };
+
+
+    // 選択されたキャラのID
+    int selectedCharacterId = -1;
 };
 
