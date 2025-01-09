a2b57dbc0cc6694ff44b585b6b5e4b56c7f6f010
ライック <88312556+raikku@users.noreply.github.com>
2025-01-09 15:47:50 +0900

    デバック情報、名前の設定、調整できるようにした

--- Source/Audio/AudioSource.cpp
+++ Source/Audio/AudioSource.cpp
@@ -5,22 +5,20 @@
 
 void AudioSource::OnGUI()
 {
-    ImGui::Text("Audio Name: %s", name);
+    ImGui::Text("Audio Name: %s", name.c_str());
+
+    ImGui::Checkbox("Loop", &isLooping);
+    ImGui::SameLine();
 
     if (ImGui::Button("Play")) {
-        Play(isLooping, volumeControl);
+            Play(isLooping, volumeControl);
     }
     ImGui::SameLine();
     if (ImGui::Button("Stop")) {
         Stop();
     }
     ImGui::SameLine();
-    ImGui::Checkbox("Loop", &isLooping);
     ImGui::SliderFloat("Volume", &volumeControl, 0.0f, 1.0f);
-
-    // 再生中のものがあれば,即時反映 チェック外してもループ抜けない
-    if (isLooping)
-        Play(isLooping, volumeControl);
 }
 
 void AudioSource::SetAudio(int id)
