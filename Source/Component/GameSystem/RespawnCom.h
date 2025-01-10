739bf6c7db2ab21c6ae609fc7c250239605b88c5
MSI\2210135 <santora5.2002@gmail.com>
2025-01-10 11:08:10 +0900

    ScenePVPにリスポーン処理いれた

--- Source/Component/GameSystem/RespawnCom.h
+++ Source/Component/GameSystem/RespawnCom.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Component/System/Component.h"
-
+#include "PVPGameSystem\PVPGameSystem.h"
 
 class RespawnCom :public Component
 {
@@ -26,18 +26,16 @@ public:
 
     std::vector<DirectX::XMFLOAT3>& GetRespawnPoses() { return respawnPoses; }
     void AddRespawnPoses(DirectX::XMFLOAT3 pos) { respawnPoses.emplace_back(pos); }
-    GameMode GetGameMode() { return gameMode; }
-    void SetGameMode(GameMode mode) { gameMode = mode; }
+    PVPGameSystem::GAME_MODE GetGameMode() { return gameMode; }
+    void SetGameMode(PVPGameSystem::GAME_MODE mode) { gameMode = mode; }
 
     bool GetIsRespawn() { return isRespawn; }
     void SetIsRespawn(bool flag) { isRespawn = flag; }
 
 private:
-    GameMode gameMode = GameMode::PVE;
+    PVPGameSystem::GAME_MODE gameMode;
 
     std::vector<DirectX::XMFLOAT3> respawnPoses = {};
     float playerDeathHeight = -50;
     bool isRespawn = false;
-
-
 };
