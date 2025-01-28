ecba9a182027d1db4d17f1a83bc8953b5df3c74c
MSI\2210454 <yito2881@gmail.com>
2025-01-28 11:29:19 +0900

    勝敗時のスロー

--- Source/Scene/ScenePVP/ScenePVP.cpp
+++ Source/Scene/ScenePVP/ScenePVP.cpp
@@ -695,6 +695,7 @@ void ScenePVP::Update(float elapsedTime)
         //ˆê‰ñ‚¾‚¯’Ê‚·
         if (!PlayerUIManager::Instance().GetIsEndFLG()) {
             PlayerUIManager::Instance().CreateGameJudgeUI(pvpGameSystem->GetVictoryTeam());
+            TimeManager::Instance().SetTimeEffect(0.35f,5.0f);
         }
 
         //‰¼‘JˆÚ
