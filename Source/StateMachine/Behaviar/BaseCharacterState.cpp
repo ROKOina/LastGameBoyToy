82dcae069cc1e8d99a79f6e725833e0874ce85ea
yusaku12 <2210359@ecc.ac.jp>
2025-01-30 23:28:11 +0900

    修正

--- Source/StateMachine/Behaviar/BaseCharacterState.cpp
+++ Source/StateMachine/Behaviar/BaseCharacterState.cpp
@@ -182,9 +182,6 @@ void BaseCharacter_MoveState::Exit()
 #pragma region Jump
 void BaseCharacter_JumpState::Enter()
 {
-    //初期設定
-    moveCom.lock()->SetAirForce(12.620);
-
     //ジャンプ
     JumpInput(owner->GetGameObject(), 1.5f);
 
@@ -209,9 +206,6 @@ void BaseCharacter_JumpState::Execute(const float& elapsedTime)
 #pragma region JumpLoop
 void BaseCharacter_JumpLoop::Enter()
 {
-    //初期設定
-    moveCom.lock()->SetAirForce(12.620);
-
     //アニメーション検索
     int animaIndex = animationCom.lock()->FindAnimation("Jump_middle");
 
@@ -308,8 +302,6 @@ void BaseCharacter_DeathState::Execute(const float& elapsedTime)
 }
 void BaseCharacter_DeathState::Exit()
 {
-    //animationCom.lock()->SetUpAnimationUpdate(AnimationCom::AnimationType::UpperLowerAnimation);
-    //animationCom.lock()->PlayAnimation(animationCom.lock()->FindAnimation("Down"), false);
 }
 #pragma endregion
 
