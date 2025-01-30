fcfc36e45edf550124a2f5af85282cabe9a5a7c9
MSI\2210454 <yito2881@gmail.com>
2025-01-30 17:34:36 +0900

    KillEffect修正

--- Source/Component/UI/PlayerUI.cpp
+++ Source/Component/UI/PlayerUI.cpp
@@ -1654,7 +1654,11 @@ void PlayerUIManager::KillLogUpdate(float elapsedTime)
                     saveCharaKilog.emplace_back(d);
                 }
             }
-            killflg = false;
+            ki
+                
+                
+                
+                llflg = false;
         }
     }
 
@@ -2154,19 +2158,15 @@ void UI_KillEffect::Start()
 
 void UI_KillEffect::Update(float elapsedTime)
 {
-    effectFLGTimer -= elapsedTime;
+   
 
     auto& player = GameObjectManager::Instance().Find("player")->GetComponent<CharacterCom>();
     for (int i = 0; i < 4; i++)
     {
         auto& killflg = StaticSendDataManager::Instance().GetKillID(player->GetNetCharaData().GetNetPlayerID(), i);
         if (killflg)
-        {
-            if (effectFLGTimer < 0)
-            {
-                effectFLG = true;
-                effectFLGTimer = 3;
-            }
+        {          
+             effectFLG = true;         
         }
     }
     EffectUpdat(elapsedTime);
