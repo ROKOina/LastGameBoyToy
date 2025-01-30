834324993c0be1852111a138ab8036fc2d7023f6
MSI\2210135 <santora5.2002@gmail.com>
2025-01-30 17:28:48 +0900

    ギミック

--- Source/Component/Stage/StageEditorCom.cpp
+++ Source/Component/Stage/StageEditorCom.cpp
@@ -207,7 +207,8 @@ void StageEditorCom::OnGUI()
                 "TowerGimic",
                 "GateGimic",
                 "SpawnGimic",
-                "Plane"
+                "Plane",
+                "JumpGimic"
             };
             int funcIndex = (int)objName.second.func;
             ImGui::Combo((char*)u8"¶¬ŠÖ”", &funcIndex, FuncName, (int)GenerateFuncName::Max);
@@ -529,5 +530,5 @@ void StageEditorCom::JumpGimic(GameObj& place)
     sphere->SetMyTag(COLLIDER_TAG::JUMPGIMMICK);
     sphere->SetJudgeTag(COLLIDER_TAG::Player);
     auto& knockback = place->AddComponent<KnockBackCom>();
-    knockback->SetKnockBackForce({ 0,10,20 });
+    knockback->SetKnockBackForce({ 0,15,0 });
 }
